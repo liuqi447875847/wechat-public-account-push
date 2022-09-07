cce704f8882c03a1fe54544718a0341620197f83
liuqi447875847 <74440746+liuqi447875847@users.noreply.github.com>
2022-09-07 22:20:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ export const config = {
       name: "我呀~", 
       id: "oiKdI69t78plYwXMbOV4vd1dr1uQ",
       useTemplateId: "_6hzt5NldBbGBi7m8FRiG6gm7PI_z8yQ0OhmSIpWIt4",
-      province: "上海",
+      province: "",
       city: "",
       openUrl: "https://weibo.com/"
     }, 
